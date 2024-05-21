c237b96f01b9554a8d7a76ef3f757e0ae1fb00d1
LRodrigues07 <32080075+LRodrigues07@users.noreply.github.com>
2024-05-21 19:08:36 -0300

    Sua mensagem de commit descrevendo as mudanças

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }  from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './componentes/Home';
+import Login from './componentes/Login';
+import Signup from './componentes/Signup';
+import './style.css'; // Importação do arquivo de estilos CSS
+import PasswordResetRequest from './Request-Password-Reset';
+import PasswordReset from './Password-Reset';
+import NavbarLogado from './componentes/Home-Logado';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login setIsAuthenticated={setIsAuthenticated} />} />
+        <Route path="/signup" element={<Signup />}/>
+        <Route path="/request-password-reset" element={<PasswordResetRequest />} />
+        <Route path="/reset-password" element={<PasswordReset />} />
+        <Route path="/reset-password" element={<PasswordReset />} />
+        <Route path="/homeLogado" element={<NavbarLogado />} />
+      </Routes>
+    </Router>
+
   );
 }
 
